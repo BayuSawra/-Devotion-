f572cff21513f2ab215ae206bb9775099bb9f84a
XHXIAIEIN <xhxiaiein@qq.com>
2024-05-06 13:49:07 +0800

    游戏预览

---
+++ Project/Preview/20240505/scripts/project/scriptsInEvents.js
@@ -0,0 +1,37 @@
+
+
+
+const scriptsInEvents = {
+
+	async 通用逻辑_Event61_Act1(runtime, localVars)
+	{
+		const c3ArrayFile = JSON.parse(localVars.source);
+		let result = {};
+		
+		for (let i = 1; i < c3ArrayFile.data[0].length; i++) {
+		    let id = c3ArrayFile.data[0][i][0];
+		    if (id && !result[id]) {
+		        result[id] = [];
+		    }
+		
+		    let obj = {};
+		    for (let j = 0; j < c3ArrayFile.data.length; j++) {
+		        let key = c3ArrayFile.data[j][0][0];
+		        let value = c3ArrayFile.data[j][i][0];
+		        if (value !== "") {
+		            obj[key] = value;
+		        }
+		    }
+		
+		    if (id && Object.keys(obj).length > 0) {
+		        result[id].push(obj);
+		    }
+		}
+		
+		runtime.setReturnValue(JSON.stringify(result));
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
